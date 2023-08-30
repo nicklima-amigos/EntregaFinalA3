1152a078d6a53d08235f8221a26ceeebd2b73761
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-30 18:51:20 -0300

    fix: rename database connection name to fix typo

--- src/backend/src/main.js
+++ src/backend/src/main.js
@@ -1,10 +1,10 @@
 // @ts-check
 
 import { App } from "./app.js";
-import { DatabasConnection } from "./infrastructure/database/connection.js";
+import { DatabaseConnection } from "./infrastructure/database/connection.js";
 
 const bootstrap = async () => {
-  const db = new DatabasConnection("./backend.db");
+  const db = new DatabaseConnection("./backend.db");
   const app = new App(db);
 
   await app.init();

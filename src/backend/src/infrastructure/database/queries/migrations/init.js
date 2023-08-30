47a6bd75387cffa43a498e11b57df61943a4adc2
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-08-30 18:46:50 -0300

    chore: remove empty line from init query

--- src/backend/src/infrastructure/database/queries/migrations/init.js
+++ src/backend/src/infrastructure/database/queries/migrations/init.js
@@ -9,7 +9,6 @@ export const init = [
         release_date DATE NOT NULL
     );`,
   `
-
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username VARCHAR(255) UNIQUE NOT NULL,

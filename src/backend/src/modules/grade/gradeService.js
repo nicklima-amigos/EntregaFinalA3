cebce6c6d305b4f58478dec8b545b2f248705e62
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-09 11:02:52 -0300

    feat: implement grades service

--- src/backend/src/modules/grade/gradeService.js
+++ src/backend/src/modules/grade/gradeService.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+import { HttpError } from "../../exceptions/httpError";
 import { PlatformsRepository } from "../platform/platformRepository";
 import { UsersRepository } from "../user/userRepository";
 import { GradesRepository } from "./gradeRepository";
@@ -18,5 +19,58 @@ export class GradesService {
 
   async find() {
     return this.gradesRepository.find();
+  }
+
+  /**
+   *
+   * @param {number} id
+   * @returns
+   */
+  async findOne(id) {
+    return this.gradesRepository.findOne(id);
+  }
+
+  /**
+   *
+   * @param {number} userId
+   * @returns
+   */
+  async findGradesByUser(userId) {
+    return this.gradesRepository.findGradesByUser(userId);
+  }
+
+  /**
+   *
+   * @param {CreateGradeDto} createGradeDto
+   * @returns
+   */
+  async create({ userId, gameId, grade }) {
+    const user = await this.usersRepository.findOne(userId);
+    if (!user) {
+      throw new HttpError(404, "User not found!");
+    }
+    const platform = await this.platformsRepository.findOne(gameId);
+    if (!platform) {
+      throw new HttpError(404, "Platform not found!");
+    }
+    return this.gradesRepository.create({ userId, gameId, grade });
+  }
+
+  /**
+   *
+   * @param {UpdateGradeDto} updateGradeDto
+   * @returns
+   */
+  async update({ id, grade }) {
+    return this.gradesRepository.update({ id, grade });
+  }
+
+  /**
+   *
+   * @param {number} id
+   * @returns
+   */
+  async delete(id) {
+    return this.gradesRepository.delete(id);
   }
 }

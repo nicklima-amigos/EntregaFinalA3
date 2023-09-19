5bf23dbc5736ec79fe65e61ebd1d7784e40e7404
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-18 22:34:42 -0300

    chore: formatting

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,53 +1,53 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginForm from './pages/LoginForm/LoginForm';
-import Platforms from './pages/Platforms/Platforms';
-import PlatformForm from './pages/PlatformForm/PlatformForm';
-import GameForm from './pages/GameForm/GameForm';
-import CategoryForm from './pages/CategoryForm/CategoryForm';
-import styles from './App.module.css';
-import SignUpForm from './pages/SignUpForm/SignUpForm';
-import { AuthContext } from './contexts/authContext';
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginForm from "./pages/LoginForm/LoginForm";
+import Platforms from "./pages/Platforms/Platforms";
+import PlatformForm from "./pages/PlatformForm/PlatformForm";
+import GameForm from "./pages/GameForm/GameForm";
+import CategoryForm from "./pages/CategoryForm/CategoryForm";
+import styles from "./App.module.css";
+import SignUpForm from "./pages/SignUpForm/SignUpForm";
+import { AuthContext } from "./contexts/authContext";
+import { useState } from "react";
 
 function App() {
   const [user, setUser] = useState(null);
 
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <LoginForm />,
     },
     {
-      path: 'signup',
+      path: "signup",
       element: <SignUpForm />,
     },
     {
-      path: 'platforms',
+      path: "platforms",
       element: <Platforms />,
     },
     {
-      path: 'platforms/create',
+      path: "platforms/create",
       element: <PlatformForm />,
     },
     {
-      path: 'games',
+      path: "games",
       element: <h1>Veja jogos aqui</h1>,
     },
     {
-      path: 'games/:id',
+      path: "games/:id",
       element: <h1>Veja detalhes de um jogo aqui</h1>,
     },
     {
-      path: 'games/create',
+      path: "games/create",
       element: <GameForm />,
     },
 
     {
-      path: 'categories',
+      path: "categories",
       element: <h1>Veja categorias aqui</h1>,
     },
     {
-      path: 'categories/create',
+      path: "categories/create",
       element: <CategoryForm />,
     },
   ]);

82f0fcf11f186d1bda0197aeb94a6b3ff2069437
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-18 23:28:36 -0300

    feat: make platform navbar full width

--- src/frontend/src/pages/Platforms/Platforms.jsx
+++ src/frontend/src/pages/Platforms/Platforms.jsx
@@ -24,8 +24,8 @@ export default function Platforms() {
     navigate(-1);
   };
   return (
-    <div className="container d-flex flex-column align-items-start h-100 w-100">
-      <div className="container d-flex align-items-center justify-content-between h-40 w-100 bg-warning">
+    <div className="d-flex flex-column align-items-start h-100 w-100">
+      <div className="d-flex align-items-center justify-content-between h-40 w-100 bg-warning">
         <h1 className="w-100">Nick Games</h1>
         <div className="container d-flex  justify-content-between h-40 w-100 ">
           <Button onClick={() => navigate("/platforms/create")}>

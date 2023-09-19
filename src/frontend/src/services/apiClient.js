78744ba63825a71842ac23c10a5353872396359b
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-09-18 21:52:09 -0300

    fix: remove axios and implement simple apiClient

--- src/frontend/src/services/apiClient.js
+++ src/frontend/src/services/apiClient.js
@@ -1,12 +1,45 @@
-import axios from 'axios';
-
 const baseURL = import.meta.env.VITE_API_URL
   ? import.meta.env.VITE_API_URL
   : 'http://localhost:3000';
 
-export const apiClient = axios.create({
-  headers: {
-    'Content-Type': 'application/json',
+export const apiClient = {
+  get: (endpoint, options = {}) => {
+    return fetchFromBaseUrl(endpoint, undefined, options);
   },
-  baseURL,
-});
+  post: (endpoint, requestData, options = {}) => {
+    return fetchFromBaseUrl(endpoint, requestData, {
+      method: 'POST',
+      ...options,
+    });
+  },
+  put: (endpoint, requestData, options = {}) => {
+    return fetchFromBaseUrl(endpoint, requestData, {
+      method: 'PUT',
+      ...options,
+    });
+  },
+  delete: (endpoint, options = {}) => {
+    return fetchFromBaseUrl(endpoint, undefined, {
+      method: 'DELETE',
+      ...options,
+    });
+  },
+};
+
+const fetchFromBaseUrl = async (endpoint, requestData, options) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    ...options.headers,
+  };
+  const body = JSON.stringify(requestData);
+  const response = await fetch(`${baseURL}${endpoint}`, {
+    headers,
+    body,
+  });
+  const status = response.status;
+  const data = await response.json();
+  return {
+    status,
+    data,
+  };
+};

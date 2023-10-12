faa4b275aadf6ba39c3c0e34503f858f78d4ae17
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-10-12 00:15:31 -0300

    fix: remove non existing import

--- src/backend/src/modules/platform/platformRepository.js
+++ src/backend/src/modules/platform/platformRepository.js
@@ -1,10 +1,8 @@
 import { createGamePlatformQuery } from "../common/queries/createGamePlatform.js";
-import Game from "../game/gameModel.js";
 import { createPlatformQuery } from "./queries/createPlatform.js";
 import { deletePlatformQuery } from "./queries/deletePlatform.js";
 import { findOnePlatformQuery } from "./queries/findOnePlatform.js";
 import { findOnePlatformByNameQuery } from "./queries/findOnePlatformByName.js";
-import { findPlatformGamesQuery } from "./queries/findPlatformGames.js";
 import { findPlatformsQuery } from "./queries/findPlatforms.js";
 import { updatePlatformQuery } from "./queries/updatePlatform.js";
 

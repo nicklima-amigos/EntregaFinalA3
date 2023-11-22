a6e1e23597f6fa0733dff84227110021ac31ac23
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-11-22 13:41:03 -0300

    feat: create reusable modal component

--- src/frontend/src/components/UI/GradeForm/GradeForm.jsx
+++ src/frontend/src/components/UI/GradeForm/GradeForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Modal from "../Modal/Modal";
 
 export default function GradeForm({ game, cleanTitle }) {
   const [grade, setGrade] = useState(0);
@@ -17,51 +18,16 @@ export default function GradeForm({ game, cleanTitle }) {
   };
 
   return (
-    <div
-      className="modal !z-n1"
-      id={cleanTitle}
-      tabIndex="-1"
-      aria-labelledby={cleanTitle}
-      aria-hidden="true"
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id={cleanTitle}>
-              Avaliar {game.title}
-            </h1>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body d-flex justify-content-around">
-            <label htmlFor="grade">Nota: </label>
-            <input
-              type="number"
-              max={10}
-              min={0}
-              value={grade}
-              id="grade"
-              onChange={handleGradeChange}
-            />
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Fechar
-            </button>
-            <button type="button" className="btn">
-              Salvar
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Modal id={cleanTitle} title={`Avaliando ${game.title}`}>
+      <label htmlFor="grade">Nota: </label>
+      <input
+        type="number"
+        max={10}
+        min={0}
+        value={grade}
+        id="grade"
+        onChange={handleGradeChange}
+      />
+    </Modal>
   );
 }

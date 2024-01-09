2db07fa1f133b8f42ac5264cfc4b8fd28a116c8f
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-01-09 19:52:05 -0300

    refactor: improve modules dependency injection logic

--- src/backend/src/modules/category/categoryModule.js
+++ src/backend/src/modules/category/categoryModule.js
@@ -1,19 +1,21 @@
 import { CategoriesController } from "./categoryController.js";
 import { CategoriesRepository } from "./categoryRepository.js";
 import { categoriesRoutes } from "./categoryRoutes.js";
-import { UsersRepository } from "../user/userRepository.js";
-import { GamesRepository } from "../game/gameRepository.js";
 import { CategoriesService } from "./categoryService.js";
 
-export const categoriesModule = (db) => {
-  const categoriesRepository = new CategoriesRepository(db);
-  const usersRepository = new UsersRepository(db);
-  const gamesRepository = new GamesRepository(db);
+export const startCategoriesModule = (db, usersRepository, gamesRepository) => {
+  const repository = new CategoriesRepository(db);
   const service = new CategoriesService(
-    categoriesRepository,
+    repository,
     usersRepository,
     gamesRepository,
   );
   const controller = new CategoriesController(service);
-  return categoriesRoutes(controller);
+  const routes = categoriesRoutes(controller);
+  return {
+    repository,
+    service,
+    controller,
+    routes,
+  };
 };

06b907fae757ab9f23a32436da9dd1b583281871
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-01-09 20:12:57 -0300

    fix: duplicated code

--- src/backend/src/modules/user/usersModule.js
+++ src/backend/src/modules/user/usersModule.js
@@ -3,17 +3,6 @@ import { UsersRepository } from "./userRepository.js";
 import { usersRoutes } from "./userRoutes.js";
 import { UsersService } from "./userService.js";
 
-export const usersModule = (db) => {
-  const repository = new UsersRepository(db);
-  const service = new UsersService(repository);
-  const controller = new UserController(service);
-  return usersRoutes(controller);
-};
-import { UserController } from "./userController.js";
-import { UsersRepository } from "./userRepository.js";
-import { usersRoutes } from "./userRoutes.js";
-import { UsersService } from "./userService.js";
-
 export const startUsersModule = (db) => {
   const repository = new UsersRepository(db);
   const service = new UsersService(repository);
